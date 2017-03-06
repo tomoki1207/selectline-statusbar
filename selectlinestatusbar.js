a1e52b8a0367068495216594583c62ef7989833e
Emery <emery@iubang.com>
2017-03-06 11:01:57 +0800

    live load with settings changed

--- selectlinestatusbar.js
+++ selectlinestatusbar.js
@@ -7,25 +7,42 @@ const
 
 class SelectLineStatusBar {
   constructor() {
-    let alignment = vscode.StatusBarAlignment.Left;
-    let alignConfig = vscode.workspace.getConfiguration('selectline').alignment;
-    if (alignConfig) {
-      if (alignConfig == 'left') {
-        alignment = vscode.StatusBarAlignment.Left;
-      } else if (alignConfig == 'right') {
-        alignment = vscode.StatusBarAlignment.Right;
-      }
-    }
-    this._statusBar = vscode.window.createStatusBarItem(alignment, vscode.workspace.getConfiguration('selectline').statusbarPriority || 100);
+    this.alignConfig = vscode.workspace.getConfiguration('selectline').alignment || 'left';
+    this.statusBarPriority = vscode.workspace.getConfiguration('selectline').statusbarPriority || 100;
+    this.displayFormat = vscode.workspace.getConfiguration('selectline').displayFormat || DEFAULT_FORMAT;
+    this._statusBar = vscode.window.createStatusBarItem(this.getAlignmentEnum(this.alignConfig), this.statusBarPriority);
     vscode.window.onDidChangeActiveTextEditor(e => e && this.displaySelectedLineCount(e.selections));
     vscode.window.onDidChangeTextEditorSelection(e => e && this.displaySelectedLineCount(e.selections));
     vscode.window.onDidChangeTextEditorViewColumn(e => e && this.displaySelectedLineCount(e.selections));
-    this._statusBar.show();
+
+    vscode.workspace.onDidChangeConfiguration(() => {
+      this.displayFormat = vscode.workspace.getConfiguration('selectline').displayFormat || DEFAULT_FORMAT;
+      if ((vscode.workspace.getConfiguration('selectline').alignment || 'left') != this.alignConfig || (vscode.workspace.getConfiguration('selectline').statusbarPriority || 100) != this.statusBarPriority) {
+        this.alignConfig = vscode.workspace.getConfiguration('selectline').alignment || 'left';
+        this.statusBarPriority = vscode.workspace.getConfiguration('selectline').statusbarPriority || 100;
+        this._statusBar.hide();
+        this._statusBar.dispose();
+        this._statusBar = vscode.window.createStatusBarItem(this.getAlignmentEnum(this.alignConfig), this.statusBarPriority);
+      }
+    });
   }
   displaySelectedLineCount(selections) {
-    console.log(selections);
     let selectedcount = selections.reduce((pre, selection) => pre + selection.end.line - selection.start.line + (selection.end.character == 0 ? 0 : 1), 0);
-    this._statusBar.text = 1 < selectedcount ? util.format(vscode.workspace.getConfiguration('selectline').displayFormat || DEFAULT_FORMAT, selectedcount) : '';
+    if (selectedcount > 1) {
+      this._statusBar.text = util.format(this.displayFormat, selectedcount);
+      this._statusBar.show();
+    } else {
+      this._statusBar.hide();
+    }
+  }
+  getAlignmentEnum(alignConfig) {
+    if (alignConfig == 'left') {
+      return vscode.StatusBarAlignment.Left;
+    } else if (alignConfig == 'right') {
+      return vscode.StatusBarAlignment.Right;
+    } else {
+      return vscode.StatusBarAlignment.Left;
+    }
   }
   dispose() {
     this._statusBar.dispose();
